c08c668ff221499138396a0ab3068227425c3388
nutujean <j@brzfreight.com>
2025-09-29 15:13:20 -0500

    Adăugat ruta /profil pentru pagina Profil

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import AdaugaAnunt from "./pages/AdaugaAnunt";
-import AnunturileMele from "./pages/AnunturileMele";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AdaugaAnunt from "./pages/AdaugaAnunt";
+import AnunturileMele from "./pages/AnunturileMele";
 import Anunturi from "./pages/Anunturi";
+import DetaliuAnunt from "./pages/DetaliuAnunt";
 import EditareAnunt from "./pages/EditareAnunt";
-import DetaliuAnunt from "./pages/DetaliuAnunt"; // ai deja fișierul acesta
+import Profil from "./pages/Profil";
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/adauga-anunt" element={<AdaugaAnunt />} />
-            <Route path="/anunturile-mele" element={<AnunturileMele />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/anunturi" element={<Anunturi />} />
-            <Route path="/editeaza-anunt/:id" element={<EditareAnunt />} />
-            <Route path="/anunt/:id" element={<DetaliuAnunt />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/adauga-anunt" element={<AdaugaAnunt />} />
+        <Route path="/anunturile-mele" element={<AnunturileMele />} />
+        <Route path="/anunturi" element={<Anunturi />} />
+        <Route path="/anunt/:id" element={<DetaliuAnunt />} />
+        <Route path="/editeaza-anunt/:id" element={<EditareAnunt />} />
+        <Route path="/profil" element={<Profil />} />
+      </Routes>
     </Router>
   );
 }
