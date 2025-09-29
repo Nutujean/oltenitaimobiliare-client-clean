988c777a545f6308a059f662c338218d800882f3
nutujean <j@brzfreight.com>
2025-09-29 15:15:09 -0500

    Integrat Footer și optimizat structura layout în App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -13,18 +14,28 @@ import Profil from "./pages/Profil";
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/adauga-anunt" element={<AdaugaAnunt />} />
-        <Route path="/anunturile-mele" element={<AnunturileMele />} />
-        <Route path="/anunturi" element={<Anunturi />} />
-        <Route path="/anunt/:id" element={<DetaliuAnunt />} />
-        <Route path="/editeaza-anunt/:id" element={<EditareAnunt />} />
-        <Route path="/profil" element={<Profil />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        {/* Navbar sus */}
+        <Navbar />
+
+        {/* Conținutul principal */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/adauga-anunt" element={<AdaugaAnunt />} />
+            <Route path="/anunturile-mele" element={<AnunturileMele />} />
+            <Route path="/anunturi" element={<Anunturi />} />
+            <Route path="/anunt/:id" element={<DetaliuAnunt />} />
+            <Route path="/editeaza-anunt/:id" element={<EditareAnunt />} />
+            <Route path="/profil" element={<Profil />} />
+          </Routes>
+        </main>
+
+        {/* Footer jos */}
+        <Footer />
+      </div>
     </Router>
   );
 }
