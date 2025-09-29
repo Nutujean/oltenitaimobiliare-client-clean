69b5b40cedf5e13a4489cd223ae24d032f1ceb0a
nutujean <j@brzfreight.com>
2025-09-29 08:55:52 -0500

    fix: adaugat rute corecte in App.jsx + functionalitate navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Search from "./pages/Search";
 import AdaugaAnunt from "./pages/AdaugaAnunt";
 import AnunturileMele from "./pages/AnunturileMele";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import ListingDetails from "./pages/ListingDetails";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
-export default function App() {
+function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <Router>
       <Navbar />
-      <main className="flex-1">
+      <div className="min-h-screen bg-gray-50">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/adauga-anunt" element={<AdaugaAnunt />} />
+          <Route path="/adauga" element={<AdaugaAnunt />} />
           <Route path="/anunturile-mele" element={<AnunturileMele />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/listing/:id" element={<ListingDetails />} />
         </Routes>
-      </main>
-      <Footer />
-    </div>
+      </div>
+    </Router>
   );
 }
+
+export default App;
