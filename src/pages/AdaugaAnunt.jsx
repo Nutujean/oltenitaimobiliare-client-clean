0ef1c94715b00982159fd02420e57f89194d8461
nutujean <j@brzfreight.com>
2025-10-27 08:26:40 -0500

    🚑 Fix: removed missing AuthContext import

--- src/pages/AdaugaAnunt.jsx
+++ src/pages/AdaugaAnunt.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext"; // asigură-te că ai acest context
 import axios from "axios";
 
 const AdaugaAnunt = () => {
