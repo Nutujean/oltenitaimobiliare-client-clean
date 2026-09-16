d871d3d9a951ffababdd0fed0cf67f9205aa9b95
Nutujean <j@brzfreight.com>
2026-09-16 10:29:49 -0500

    Lazy-load listing images on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -675,6 +675,8 @@ export default function Home() {
                             <img
                               src={l.images[0]}
                               alt={l.title}
+                              loading="lazy"
+                              decoding="async"
                               className={
                                 "w-full h-full object-cover " +
                                 (isSold
@@ -772,6 +774,8 @@ export default function Home() {
                         <img
                           src={l.images[0]}
                           alt={l.title}
+                          loading="lazy"
+                          decoding="async"
                           className={
                             "w-full h-56 object-cover " +
                             (isSold
